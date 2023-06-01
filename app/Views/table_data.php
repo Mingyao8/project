f923c76d81367d49fc7b36f7d003efed379c9149
MingYao <nunu0831@gmail.com>
2023-06-01 17:46:59 +0800

    Initial commit

---
+++ app/Views/table_data.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>
+        <?= esc($title) ?>
+    </title>
+
+</head>
+
+<body>
+    <style>
+        table,
+        td {
+            border: 1px solid #333;
+        }
+
+        thead,
+        tfoot {
+            background-color: #333;
+            color: #fff;
+        }
+    </style>
+    <h1>
+        <?= esc($heading) ?>
+    </h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Time Used</th>
+                <th>Step</th>
+                <th>Description</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($todo_list as $weeks => $values): ?>
+                <tr>
+                    <td>
+                        <?= esc($weeks) ?>
+                    </td>
+                    <?php foreach ($values as $things): ?>
+                        <td>
+                            <?= esc($things) ?>
+                        </td>
+                    <?php endforeach ?>
+                </tr>
+            <?php endforeach ?>
+
+        </tbody>
+    </table>
+
+
+</body>
+
+</html>
